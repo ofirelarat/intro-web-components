b3ff579ee7d487df15ad659727957ecfa7acce40
ofirelarat <96ofir11@gmail.com>
2023-06-24 19:54:39 +0300

    make it intro component

--- src/intro/index.js
+++ src/intro/index.js
@@ -1,18 +1,28 @@
 export class Intro extends HTMLElement {
     constructor() {
       super();
-  
+
       const template = document.createElement('template');
-      template.innerHTML = `
-        <div>some check</div>
-      `;
+      if(!this.getIsAlreadyRendered()) {
+        template.innerHTML = `
+          <div>some check</div>
+        `;
+      }
   
       const shadowRoot = this.attachShadow({ mode: 'open' });
       shadowRoot.appendChild(template.content.cloneNode(true));
     }
   
     connectedCallback() {
-        console.log('callback')
+      console.log('callback')
+      if(!this.getIsAlreadyRendered()) {
+        window.localStorage.setItem('intro-component', new Date());
+      }
+    }
+
+    getIsAlreadyRendered() {
+      const introStorage = window.localStorage.getItem('intro-component');
+      return introStorage != null;
     }
   }
   
