831680904126fe2909b097629321d3d7762d4a91
ofirelarat <96ofir11@gmail.com>
2023-06-26 22:18:58 +0300

    new version

--- src/intro/index.js
+++ src/intro/index.js
@@ -1,33 +1,38 @@
 export class Intro extends HTMLElement {
-    constructor() {
-      super();
+  constructor() {
+    super();
 
-      const context = this.getAttribute('context');
-      this.context_name = `intro-component-${context}`;
-      const template = document.createElement('template');
+    const template = document.createElement('template');
 
-      if(!this.getIsAlreadyRendered()) {
-        template.innerHTML = `
-          <div id='intro-container'><slot></slot></div>
+    template.innerHTML = `
+          <div id='intro-container'></div>
         `;
-      }
 
-      const shadowRoot = this.attachShadow({ mode: 'open' });
-      shadowRoot.appendChild(template.content.cloneNode(true));
-    }
-  
-    connectedCallback() {
-      if(!this.getIsAlreadyRendered()) {
-        window.localStorage.setItem(this.context_name, new Date());
-      }
-    }
+    const shadowRoot = this.attachShadow({ mode: 'open' });
+    shadowRoot.appendChild(template.content.cloneNode(true));
+  }
 
-    getIsAlreadyRendered() {
-      const introStorage = window.localStorage.getItem(this.context_name);
+  connectedCallback() {
+    const context = this.getAttribute('context');
+    this.context_name = `intro-component-${context ?? '1'}`;
 
-      return introStorage != null;
+    if (!this.getIsAlreadyRendered()) {
+      window.localStorage.setItem(this.context_name, new Date());
+      this.shadowRoot.querySelector('#intro-container').innerHTML = `<slot></slot>`
+    } else {
+      this.shadowRoot.querySelector('#intro-container').innerHTML = ``
     }
   }
-  
-  customElements.define('intro-container', Intro);
-  
+
+  getIsAlreadyRendered() {
+    let introStorage = null;
+    if (this.context_name) {
+      introStorage = window.localStorage.getItem(this.context_name);
+    }
+
+    return introStorage != null;
+  }
+}
+
+customElements.define('intro-container', Intro);
+
